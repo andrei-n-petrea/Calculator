7bad1f8006280a93fdf3eb7a1308f0468db8e6cc
motam <motam@ToniG>
2024-11-15 16:34:49 +0200

    hello world

---
+++ Laptop/src/main/java/com/mycompany/laptop/Laptop1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.laptop;
+
+/**
+ *
+ * @author motam
+ */
+public class Laptop1 {
+    
+}
