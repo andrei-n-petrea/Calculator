c0e7af73d0aaa4ca7d993262f26da97cf5477a6e
motam <motam@ToniG>
2024-11-22 18:49:14 +0200

    hello world

---
+++ Laptop/src/main/java/com/mycompany/laptop/OperatiuniCalculator.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.calculator;
+
+public interface OperatiuniCalculator {
+    void pornire();
+    void oprire();
+}
